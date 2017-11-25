058fe0fd293c58e30cf2b72c595f6c0e296a47a4
Gabriel Laden <gabrielnexus6p@gmail.com>
2017-11-25 12:58:54 +0700

    fixes

--- media.py
+++ media.py
@@ -1,7 +1,16 @@
 class Movie():
     """ This class provides a way to store movie related information """
 
-    def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
+    def __init__(self, title, storyline,
+                 poster_image_url, trailer_youtube_url):
+        """
+        Args:
+            title (str): movie title
+            storyline (str): summary of movie plot
+            poster_image_url (str): link to image to display
+            trailer_youtube_url (str): link to youtube video
+
+        """
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster_image_url
